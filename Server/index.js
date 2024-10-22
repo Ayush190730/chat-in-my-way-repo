1d7ceb841a461255f50c0d999f60a4be7ca96272
Ayush190730 <148128641+Ayush190730@users.noreply.github.com>
2024-10-22 09:01:26 +0530

    Server/.env

--- Server/index.js
+++ Server/index.js
@@ -22,9 +22,8 @@ app.use(cors({
     credentials: true, // If you need to send cookies or auth headers
 }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+    res.header('Access-Control-Allow-Origin', '*');
+ 
   });
 
 //Routes
